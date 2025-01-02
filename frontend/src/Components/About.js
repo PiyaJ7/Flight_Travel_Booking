49c50254592dbca9bb30bccb119faabee66997be
E M P J De Saram <jayodya7desaram@gmail.com>
2025-01-02 23:09:11 +0530

    Update responsive

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -6,6 +6,10 @@ import { RiNumber4 } from "react-icons/ri";
 import videoFile from "./videos/travel.mp4";
 import { FaPlaneUp } from "react-icons/fa6";
 import Booking from "./Booking";
+import { IoSearch } from "react-icons/io5";
+import { TbTransformFilled } from "react-icons/tb";
+import { GiChoice } from "react-icons/gi";
+import { MdBorderColor } from "react-icons/md";
 
 export default function About() {
   return (
@@ -16,29 +20,32 @@ export default function About() {
         These are the simple procedures that will make your purchasing process
         super easy and smooth. And will help you to get flight easily.
       </p>
-      <div className="flex">
+      <div className="about-step-container-div flex">
         <div className="steps-container">
           <div className="steps flex items-center">
             <RiNumber1 className="no_icon" />
-            <div className="black-dot"></div>
+            <IoSearch className="about-list-icon" />
             <p>Search Flights</p>
           </div>
           <div className="steps flex items-center">
             <RiNumber2 className="no_icon" />
-            <div className="black-dot"></div>
+            <TbTransformFilled className="about-list-icon" />
             <p>Fill Contact Form</p>
           </div>
           <div className="steps flex items-center">
             <RiNumber3 className="no_icon" />
-            <div className="black-dot"></div>
+            <GiChoice className="about-list-icon" />
             <p>Choose your Airline</p>
           </div>
           <div className="steps flex items-center">
             <RiNumber4 className="no_icon" />
-            <div className="black-dot"></div>
+            <MdBorderColor className="about-list-icon" />
             <p>Booking in 10 minutes!</p>
           </div>
-          <FaPlaneUp className="plane-icon" />
+          <FaPlaneUp
+            style={{ transform: "rotate(180deg)", transition: "0.5s" }}
+            className="plane-icon"
+          />
         </div>
         <div className="about-video flex items-center justify-center">
           <video width="60%" controls>
